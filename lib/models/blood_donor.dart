504e57dfe8a63af220e9e21a1f6dfc7283a0aa13
hiepc <ch.dhiep@gmail.com>
2022-06-03 22:48:17 +0700

    Add models

---
+++ lib/models/blood_donor.dart
@@ -0,0 +1,27 @@
+class Donor {
+  String name;
+  String contact;
+  String latitute;
+  String longitude;
+  String location;
+  String blood;
+
+  Donor(
+      {this.name,
+        this.contact,
+        this.latitute,
+        this.longitude,
+        this.location,
+        this.blood});
+  Map<String, dynamic> toMap() {
+    return {
+      'Name': name,
+      'Contact No': contact,
+      //'Location': location,
+      'Latitute': latitute,
+      'Longitude': longitude,
+      'Location': location,
+      'Blood Group': blood,
+    };
+  }
+}
