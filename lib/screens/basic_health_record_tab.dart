64794bfd9fdf0d193b813e1346264b1ac9cae14b
Trí Trần <20520327@gm.uit.edu.vn>
2022-06-05 14:56:43 +0700

    Add screen basic health record and diagnosis record tab

---
+++ lib/screens/basic_health_record_tab.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:medicalrecordapp/components/custom_dropdown_menu.dart';
+import 'package:medicalrecordapp/components/custom_text_field.dart';
+import 'package:medicalrecordapp/components/rounded_button.dart';
+import 'package:medicalrecordapp/constants.dart';
+import 'package:medicalrecordapp/models/basic_health.dart';
+import 'package:medicalrecordapp/screens/qr_code_screen.dart';
+import 'package:medicalrecordapp/services/EHR.dart';
+import 'package:medicalrecordapp/services/authenticate.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+class BasicHealthRecordTab extends StatefulWidget {
+  @override
+  _BasicHealthRecordTabState createState() => _BasicHealthRecordTabState();
+}
+
+class _BasicHealthRecordTabState extends State<BasicHealthRecordTab> {
+  bool loadingIndicator = false;
+  final ehrDatabase = new EHR(uid: Auth().getUID());
+  final height = new TextEditingController();
+  final weight = new TextEditingController();
+  String sugarLevel = '';
+  String rbcCount = '';
+  String wbcCount = '';
+  String bloodPressure = '';
+
+  Future<void> loadCurrentRecord() async {
+    final uid = Auth().getUID();
+    final latestRecord = await EHR(uid: uid).getRecord();
+    setState(() {
+      height.text = latestRecord.height;
+      weight.text = latestRecord.weight;
+      sugarLevel = latestRecord.sugerLevel;
+      rbcCount = latestRecord.rbc;
+      wbcCount = latestRecord.wbc;
+      bloodPressure = latestRecord.bp;
+    });
+  }
+
+  Future<void> _submit() async {
+    final _height = height.text;
+    final _weight = weight.text;
+    final _sugerLevel = sugarLevel;
+    final _rbcCount = rbcCount;
+    final _bloodPressure = bloodPressure;
+    final _wbcCount = wbcCount;
+    final _count = await ehrDatabase.getCount();
+    basicRecord _record = new basicRecord(
+      height: _height,
+      weight: _weight,
+      sugerLevel: _sugerLevel,
+      bp: _bloodPressure,
+      rbc: _rbcCount,
+      wbc: _wbcCount,
+      count: _count,
+    );
+    print(_record.toMap());
+    ehrDatabase.createRecord(_record);
+  }
+
+  @override
+  void initState() {
+    setState(() {
+      loadingIndicator = true;
+    });
+    loadCurrentRecord();
+    setState(() {
+      loadingIndicator = false;
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(12),
+        child: ModalProgressHUD(
+          color: Colors.white,
+          opacity: 0.9,
+          progressIndicator: kWaveLoadingIndicator,
+          inAsyncCall: loadingIndicator,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                CustomTextField(
+                  label: "Height",
+                  hint: "In Centimeters",
+                  controller: height,
+                  keyboardType: TextInputType.number,
+                ),
+                CustomTextField(
+                  label: "Weight",
+                  hint: "In Kilograms",
+                  controller: weight,
+                  keyboardType: TextInputType.number,
+                ),
+                CustomDropdownMenu(
+                    label: 'Sugar Level',
+                    initialValue: sugarLevel == '' ? null : sugarLevel,
+                    onChanged: (value) {
+                      setState(() {
+                        sugarLevel = value;
+                      });
+                    },
+                    items: [
+                      '~70-140 mg/dL',
+                      '~141-200 mg/dL',
+                      '>200 mg/dL',
+                    ]),
+                CustomDropdownMenu(
+                    label: 'RBC Count',
+                    initialValue: rbcCount == '' ? null : rbcCount,
+                    onChanged: (value) {
+                      setState(() {
+                        rbcCount = value;
+                      });
+                    },
+                    items: [
+                      'less than 4.7 mcL',
+                      '~4.7-6.1 mcL',
+                      'higher than 6.1 mcL'
+                    ]),
+                CustomDropdownMenu(
+                    label: 'WBC Count',
+                    initialValue: wbcCount == '' ? null : wbcCount,
+                    onChanged: (value) {
+                      setState(() {
+                        wbcCount = value;
+                      });
+                    },
+                    items: [
+                      '~9,000-30,000 mcL',
+                      '~6,200-17,000 mcL',
+                      '~5,000-10,000 mcL'
+                    ]),
+                CustomDropdownMenu(
+                    label: 'Blood Pressure(Sys/Dias)',
+                    initialValue: bloodPressure == '' ? null : bloodPressure,
+                    onChanged: (value) {
+                      setState(() {
+                        bloodPressure = value;
+                      });
+                    },
+                    items: [
+                      '120/80',
+                      '(120-129)/(<80)',
+                      '(130-139)/(80-89)',
+                      '140/90',
+                      '180/120',
+                    ]),
+                Container(
+                  child: RoundedButton(
+                    onPressed: () {
+                      _submit();
+                    },
+                    text: 'Update',
+                    color: Colors.green[700],
+                  ),
+                ),
+                Container(
+                  child: RoundedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => QrCodeScreen(
+                            appBarTitle: 'Share Private Data',
+                            qrCodeData: 'LifeLineShare_'+Auth().getUID(),),
+                        ),
+                      );
+                    },
+                    text: 'Share',
+                    color: Colors.green[700],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/diagnosis_record_tab.dart
@@ -0,0 +1,105 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:medicalrecordapp/components/diagnosis_card_list.dart';
+import 'package:medicalrecordapp/constants.dart';
+import 'package:medicalrecordapp/models/diagnosis.dart';
+import 'package:medicalrecordapp/services/EHR.dart';
+import 'package:medicalrecordapp/services/authenticate.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+class DiagnosisRecordTab extends StatefulWidget {
+  @override
+  _DiagnosisRecordTabState createState() => _DiagnosisRecordTabState();
+}
+
+class _DiagnosisRecordTabState extends State<DiagnosisRecordTab> {
+  bool loadingIndicator = true;
+  // List<Diagnosis> diagnosisList = [
+  //   Diagnosis(
+  //     type: 'Disease',
+  //     problem: 'COVID19',
+  //     date: 'December, 2020',
+  //     verified: true,
+  //     verifiedBy: 'Dr. Corona Chang',
+  //   ),
+  //   Diagnosis(
+  //     type: 'Accident',
+  //     problem: 'Thorax Fracture',
+  //     date: 'November, 2020',
+  //   ),
+  //   Diagnosis(
+  //     type: 'Accident',
+  //     problem: 'Leg Fracture',
+  //     date: 'January, 2019',
+  //   ),
+  // ];
+
+// Dialo
+  final erhRecord = EHR(uid: Auth().getUID());
+  CollectionReference refference;
+  QuerySnapshot snapshot;
+
+  List<Diagnosis> diagnosisList;
+
+  Future<List<Diagnosis>> fetchHistory() async {
+    List<Diagnosis> _diagnosisList = [];
+    snapshot = await erhRecord.historySnap();
+    for (int i = 0; i < snapshot.docs.length; i++) {
+      var _history = snapshot.docs[i];
+      print(_history.data());
+      _diagnosisList.add(new Diagnosis(
+        type: _history['Type'],
+        date: _history['Date'],
+        problem: _history['Problem'],
+        verified: _history['Verified'],
+        verifiedBy: _history['VerifiedBy'],
+      ));
+    }
+    setState(() {
+      diagnosisList = _diagnosisList;
+    });
+    return diagnosisList;
+  }
+
+  void nonAsync(){
+    fetchHistory();
+  }
+  @override
+  void initState() {
+    diagnosisList = [];
+    nonAsync();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //fetchHistory();
+    loadingIndicator = false;
+    return ModalProgressHUD(
+      color: Colors.white,
+      opacity: 0.9,
+      progressIndicator: kWaveLoadingIndicator,
+      inAsyncCall: loadingIndicator,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              child: DiagnosisCardList(
+                diagnosisList: diagnosisList,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
