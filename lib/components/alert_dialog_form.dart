47b538b010c50cf8d8b8e972993130479c67d378
Trí Trần <20520327@gm.uit.edu.vn>
2022-06-05 15:02:10 +0700

    Add alert dialog, verifiable diagnosis card, verifiable diagnosis card list component

---
+++ lib/components/alert_dialog_form.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:medicalrecordapp/components/custom_dropdown_menu.dart';
+import 'package:medicalrecordapp/components/custom_text_field.dart';
+import 'package:medicalrecordapp/constants.dart';
+import 'package:medicalrecordapp/models/diagnosis.dart';
+import 'package:medicalrecordapp/services/EHR.dart';
+import 'package:medicalrecordapp/services/authenticate.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+class AlertDialogForm extends StatefulWidget {
+  final BuildContext context;
+
+  AlertDialogForm({
+    @required this.context,
+  });
+
+  @override
+  _AlertDialogFormState createState() => _AlertDialogFormState();
+}
+
+class _AlertDialogFormState extends State<AlertDialogForm> {
+  bool loadingIndicator = false;
+  bool active = true;
+  String type = '';
+  TextEditingController problemController = new TextEditingController();
+  TextEditingController dateController = new TextEditingController();
+  final ehrDatabase = new EHR(uid: Auth().getUID());
+
+  Future<void> _submit() async {
+    final _date = dateController.text;
+    final _type = type;
+    final _problem = problemController.text;
+    final _verified = false;
+    final _verifiedBy = "";
+
+    Diagnosis _diagnosis = new Diagnosis(
+      date: _date,
+      problem: _problem,
+      type: _type,
+      verified: _verified,
+      verifiedBy: _verifiedBy,
+    );
+    await ehrDatabase.createDiagnosis(_diagnosis);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ModalProgressHUD(
+      color: Colors.white,
+      opacity: 0.9,
+      progressIndicator: kWaveLoadingIndicator,
+      inAsyncCall: loadingIndicator,
+      child: AlertDialog(
+        contentPadding: EdgeInsets.all(6),
+        title: Text(
+          'New Diagnosis',
+          style: kTextStyle.copyWith(
+            fontSize: 24,
+          ),
+        ),
+        content: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CustomDropdownMenu(
+                label: 'Type',
+                items: [
+                  'Disease',
+                  'Accident',
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    type = value;
+                  });
+                },
+              ),
+              CustomTextField(
+                label: 'Description',
+                hint: 'Diagnosed problem',
+                controller: problemController,
+                keyboardType: TextInputType.text,
+              ),
+              CustomTextField(
+                label: 'Date',
+                hint: 'MM, YYYY',
+                controller: dateController,
+                keyboardType: TextInputType.text,
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () async {
+              active = false;
+              FocusScope.of(context).unfocus();
+              print("Before");
+              setState(() {
+                loadingIndicator = true;
+              });
+              await _submit();
+              setState(() {
+                loadingIndicator = false;
+              });
+              print("After");
+              setState(() {});
+              Navigator.pop(context);
+              Navigator.pop(context);
+            },
+            child: Text(
+              'Submit',
+              style: kTextStyle.copyWith(color: Colors.green, fontSize: 18),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/verifiable_diagnosis_card.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:medicalrecordapp/constants.dart';
+import 'package:medicalrecordapp/models/diagnosis.dart';
+import 'package:medicalrecordapp/screens/qr_code_screen.dart';
+import 'package:medicalrecordapp/services/authenticate.dart';
+
+class VerifiableDiagnosisCard extends StatelessWidget {
+  // TODO: Create a new model diagnosis and pass it as its parameter
+  final Diagnosis diagnosis;
+  final uid = Auth().getUID();
+  VerifiableDiagnosisCard({this.diagnosis});
+  Widget getTickIfNotVerified(BuildContext context) {
+    if (!this.diagnosis.verified) {
+      return IconButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => QrCodeScreen(
+                // TODO: Replace qrCodeData with real data (UID + DiagnosisID) => this will provide a screen to the doctor with the corresponding diagnosis data
+                  appBarTitle: 'Verify Report',
+                  qrCodeData:
+                  'LIFELINEDIAGNOSIS' + '_' + diagnosis.id.toString() +'_'+Auth().getUID().toString()),
+            ),
+          );
+          print('${diagnosis.id} ${Auth().getUID()}');
+        },
+        icon: Icon(
+          Icons.check,
+          color: Colors.green[900],
+        ),
+      );
+    } else {
+      return SizedBox(
+        height: 1,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(24),
+      margin: EdgeInsets.fromLTRB(12, 12, 12, 12),
+      decoration: BoxDecoration(
+        color: this.diagnosis.verified ? Colors.green[100] : Colors.green[50],
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 3, 0, 3),
+                  child: Text(
+                    this.diagnosis.type,
+                    style: kTextStyle.copyWith(
+                      fontSize: 16,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 3, 0, 3),
+                  child: Text(
+                    this.diagnosis.problem,
+                    style: kTextStyle.copyWith(
+                      fontSize: 36,
+                      color: Colors.green[900],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 3, 0, 3),
+                  child: Text(
+                    this.diagnosis.date,
+                    style: kTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 3, 0, 3),
+                  child: Text(
+                    'Verified By: ' +
+                        (this.diagnosis.verified
+                            ? this.diagnosis.verifiedBy
+                            : 'Not yet verified'),
+                    style: kTextStyle.copyWith(
+                      fontSize: 16,
+                      color: Colors.black45,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: getTickIfNotVerified(context),
+          ),
+        ],
+      ),
+    );
+  }
+}
