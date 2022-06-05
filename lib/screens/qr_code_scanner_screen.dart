456821dd67fd9ddb7a61421e761bd9d7a47c7d6d
namkhanh685 <trannamkhanh685@gmail.com>
2022-06-05 15:13:37 +0700

    Update QR Scanner

---
+++ lib/screens/qr_code_scanner_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:medicalrecordapp/components/rounded_button.dart';
+
+class QrCodeScannerScreen extends StatefulWidget {
+  static String id = "qr_code_scanner";
+
+  @override
+  _QrCodeScannerScreenState createState() => _QrCodeScannerScreenState();
+}
+
+class _QrCodeScannerScreenState extends State<QrCodeScannerScreen> {
+  String qrCodeResult;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 0,
+        title: Row(
+          children: [
+            Hero(
+              tag: 'logo',
+              child: Container(
+                height: 40.0,
+                child: Image.asset(
+                  'assets/images/lifeline_logo.png',
+                ),
+              ),
+            ),
+            Text(
+              'Scan QR Code',
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Nexa Bold',
+                fontSize: 24,
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.white,
+        shadowColor: Colors.black54,
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            RoundedButton(
+              text: 'Open Camera',
+              color: Colors.green[900],
+              onPressed: () async {
+                String codeScanner = await FlutterBarcodeScanner.scanBarcode(
+                  '#ff6666', 'Cancel', true, ScanMode.QR);
+                setState(() {
+                  // TODO: Send data to Record Verification or
+                  qrCodeResult = codeScanner;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
